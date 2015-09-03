a72c2a34b88fa1f79e2893301ed9cc37a8135163
Sripadmanaban Subramanian <sripadmanabans@gmail.com>
2015-09-03 12:41:43 -0500

    Added model classes.

---
+++ src/model/Response.java
@@ -0,0 +1,50 @@
+package model;
+
+import com.google.gson.annotations.SerializedName;
+import model.part.*;
+
+import java.util.ArrayList;
+
+/**
+ * This class holds the response for the api.
+ * Created by Sripadmanaban on 9/3/2015.
+ */
+public class Response {
+
+    @SerializedName("coord")
+    private Coordinates coordinates;
+
+    @SerializedName("weather")
+    private ArrayList<Weather> weathers;
+
+    @SerializedName("base")
+    private String baseStation;
+
+    @SerializedName("main")
+    private Main main;
+
+    @SerializedName("wind")
+    private Wind wind;
+
+    @SerializedName("rain")
+    private Rain rain;
+
+    @SerializedName("clouds")
+    private Clouds clouds;
+
+    @SerializedName("dt")
+    private long date;
+
+    @SerializedName("sys")
+    private SystemDetails systemDetails;
+
+    @SerializedName("id")
+    private long id;
+
+    @SerializedName("name")
+    private String cityName;
+
+    @SerializedName("cod")
+    private int cod;
+
+}
---
+++ src/model/part/Weather.java
@@ -0,0 +1,23 @@
+package model.part;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * This contains details about weather.
+ * Created by Sripadmanaban on 9/3/2015.
+ */
+public class Weather {
+
+    @SerializedName("id")
+    private long id;
+
+    @SerializedName("main")
+    private String main;
+
+    @SerializedName("description")
+    private String description;
+
+    @SerializedName("icon")
+    private String icon;
+
+}
